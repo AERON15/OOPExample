89e60b3251b2256f7c6610478002b7327a1c21ba
AERON15 <aaronmulawan@gmail.com>
2025-09-02 15:20:40 +0800

    Update OOPExample.java

--- OOPExample.java
+++ OOPExample.java
@@ -1,22 +1,22 @@
-// 1. Encapsulation: Keep info safe inside the Fruit class
+
 class Fruit {
-    private String color;  // We hide the color
+    private String color; 
 
     public void setColor(String color) {
-        this.color = color;  // Set color safely
+        this.color = color;  
     }
 
     public String getColor() {
-        return color;  // Get color safely
+        return color;  
     }
 
-    // 2. Abstraction: We say fruit can be eaten, but don't say how here
+   
     void eat() {
         System.out.println("You eat the fruit.");
     }
 }
 
-// 3. Inheritance: Apple and Carrot are kinds of Fruit
+
 class Apple extends Fruit {
     @Override
     void eat() {
@@ -31,15 +31,16 @@ class Carrot extends Fruit {
     }
 }
 
-// 4. Polymorphism: One fruit type, many forms!
+
 public class OOPExample {
     public static void main(String[] args) {
         Fruit myFruit1 = new Apple();
         myFruit1.setColor("red");
-        myFruit1.eat();  // Output: You bite the red apple.
+        myFruit1.eat();  
 
         Fruit myFruit2 = new Carrot();
         myFruit2.setColor("orange");
-        myFruit2.eat();  // Output: You crunch the orange carrot.
+        myFruit2.eat(); 
     }
 }
+
